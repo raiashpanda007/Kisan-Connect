14b45801de4068170865b65b3bfeb4833f99317b
Ashwin Rai <ashwin.2201098cs@iiitbh.ac.in>
2024-09-05 13:11:03 +0530

    commit  for frontend

---
+++ BackEnd/server.js
@@ -0,0 +1,12 @@
+import dotenv, { config } from 'dotenv';
+
+
+dotenv.config();
+
+import app from './app.js';
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Store/Store.js";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} />,
+  )
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+  <RouterProvider router={router} />
+  </Provider>
 )
