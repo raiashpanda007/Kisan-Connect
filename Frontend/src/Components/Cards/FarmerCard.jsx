a0a221e97fa3a9c6ad55773c4a9ee46171c8f406
suraj-78 <surajpandit591@gmail.com>
2024-09-07 14:55:09 +0530

    redifined Farmer home page

--- Frontend/src/Components/Cards/FarmerCard.jsx
+++ Frontend/src/Components/Cards/FarmerCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const FarmerCard = ({ content, isButton }) => {
+const FarmerCard = ({ content, isButton,onClick }) => {
   return (
-    <div className="bg-gradient-to-r from-green-100 via-blue-200 to-purple-300 border-2 border-gray-300 rounded-lg p-8 w-96 shadow hover:shadow-lg transition-shadow duration-200 ease-in-out flex justify-center items-center">
+    <div onClick={onClick} className="bg-gradient-to-r from-green-100 via-blue-200 to-purple-300 border-2 border-gray-300 rounded-lg p-8 w-96 shadow hover:shadow-lg transition-shadow duration-200 ease-in-out flex justify-center items-center">
       {isButton ? (
         <button className="bg-transparent  py-6 px-12 text-xl font-semibold rounded-lg hover:border-gradient-to-r hover:from-green-600 hover:to-green-700 transition-all duration-300 ease-in-out shadow-md">
           {content}
